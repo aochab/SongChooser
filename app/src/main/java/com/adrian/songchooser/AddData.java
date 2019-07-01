10e8142c31be1acff18d68e017b06b9512c39085
Adrian Ochab <ochabadrian1@gmail.com>
2019-07-01 17:11:01 +0200

    Added menu appearance to MainActivity

--- app/src/main/java/com/adrian/songchooser/AddData.java
+++ app/src/main/java/com/adrian/songchooser/AddData.java
@@ -3,18 +3,12 @@ package com.adrian.songchooser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button addDataButton;
+public class AddData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_data);
     }
 }
-
-
-//androidx.constraintlayout.widget.ConstraintLayout
